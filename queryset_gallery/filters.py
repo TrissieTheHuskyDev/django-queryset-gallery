a103134a5a2db3853eb50a13d714d32d22475cda
Eugene Mozge <eumozge@gmail.com>
2019-06-08 18:13:40 +0300

    Refactoring: filters.py

--- queryset_gallery/filters.py
+++ queryset_gallery/filters.py
@@ -5,20 +5,21 @@ class Filter(abc.ABC):
     """Use for creating filter classes
 
     Args:
-        `filter_key` (str): the unique filter identification
+        `key` (str): the unique filter identification
 
     Methods:
         `apply`: apply filter for objects
-        `apply_dict_params`: the same as `apply` but get filter param from params dict
+        `apply_from_dict_params`: the same as `apply` but get filter param from params dict
     """
-    filter_key = ''
+    def __init__(self, key):
+        self.key = ''
 
     def apply(self, objects, param):
         return self._execute(objects, param)
 
-    def apply_dict_params(self, objects, params: dict):
-        if self.filter_key in params:
-            objects = self._execute(objects, params[self.filter_key])
+    def apply_from_dict_params(self, objects, params: dict):
+        if self.key in params:
+            objects = self._execute(objects, params[self.key])
         return objects
 
     @abc.abstractmethod
@@ -36,12 +37,13 @@ class QuerySetFilter(Filter):
 
     Usage:
         ```
-        UserEmailFilter = QuerySetFilter('user_is_active', 'user_is_active')
+        UserEmailFilter = QuerySetFilter('is_active', 'user_is_active')
         users = UserEmailFilter.apply(users, 'True')
         ```
     """
-    filter_key = ''
-    lookup = ''
+    def __init__(self, key, lookup):
+        super().__init__(key)
+        self.lookup = lookup
 
     def _execute(self, queryset, param):
         return queryset.filter(**{self.lookup: param})
