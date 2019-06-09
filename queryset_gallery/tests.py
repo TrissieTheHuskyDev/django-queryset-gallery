0b408aa5b6111f2ffa075f7922147ec07d42cca1
Eugene Mozge <eumozge@gmail.com>
2019-06-09 16:27:50 +0300

    Add tests for galleries

--- queryset_gallery/tests.py
+++ queryset_gallery/tests.py
@@ -1,19 +1,25 @@
 import unittest
+
+from queryset_gallery.filters import Filter
+from queryset_gallery.gallery import Gallery
 from queryset_gallery.paginator import Paginator
 
 
 def get_pagination_data(objects_count, page_number, page_count, per_page, errors=False):
     p = {
-        'objects_count': objects_count,
         'page_number': page_number,
+        'per_page': per_page,
         'page_count': page_count,
-        'per_page': per_page
+        'objects_count': objects_count,
     }
     not errors or p.update({'errors': 'Page data is invalid'})
     return p
 
 
 class TestPaginator(unittest.TestCase):
+
+    def setUp(self):
+        self. objects = [i for i in range(1, 11)]
 
     def test_is_page_valid(self):
         params = ((1, 1, 8, 8, True),  (4, 2, 4, 8, True),  (5, 2, 5, 9, True), (0, 5, 2, 8, False),
@@ -39,28 +45,66 @@ class TestPaginator(unittest.TestCase):
             )
 
     def test_get_objects_for_page(self):
-        objects = [i for i in range(1, 11)]
-        params = ((1, 1, [1]), (10, 1, [10]), (5, 2, [9, 10]), (4, 3, [10]), (1, 10, objects))
+        params = ((1, 1, [1]), (10, 1, [10]), (5, 2, [9, 10]), (4, 3, [10]), (1, 10, self.objects))
         for p in params:
-            paginator = Paginator(objects=objects, per_page=p[1])
+            paginator = Paginator(objects=self.objects, per_page=p[1])
             self.assertEqual(
                 paginator._get_objects_for_page(page_number=p[0]), p[2],
                 msg=p
             )
 
     def test_get_page(self):
-        objects = [i for i in range(1, 11)]
         params = (
             (1, 1, ([1], get_pagination_data(10, 1, 10, 1))),
             (0, 3, ([], get_pagination_data(10, 0, 4, 3, errors=True))),
             (2, 3, ([4, 5, 6], get_pagination_data(10, 2, 4, 3)))
         )
         for p in params:
-            paginator = Paginator(objects=objects, per_page=p[1])
+            paginator = Paginator(objects=self.objects, per_page=p[1])
             self.assertEqual(
                 paginator.get_page(page_number=p[0]), p[2],
                 msg=p
             )
+
+
+class TestGallery(unittest.TestCase):
+
+    def setUp(self):
+        class RemoveNumberFilter(Filter):
+            def _execute(self, objects, param):
+                return [i for i in objects if i != param]
+
+        self.filter = RemoveNumberFilter(key='remove')
+
+        class RemoveNumberGallery(Gallery):
+            filters = [self.filter]
+
+        self.gallery = RemoveNumberGallery()
+
+    def test_apply_filters(self):
+        self.assertEqual(self.filter.apply(objects=[1, 2, 3], param=3), [1, 2])
+        self.assertEqual(
+            self.filter.apply_from_dict_params(
+                objects=[1, 3, 3], params={'remove': 3}
+            ), [1]
+        )
+        self.assertEqual(
+            self.filter.apply_from_dict_params(
+                objects=[1, 3, 3], params={'number': 3}
+            ), [1, 3, 3]
+        )
+
+    def test_get_page(self):
+        self.assertEqual(
+            self.gallery.get_page(
+                objects=[1, 3, 2], filter_params={'remove': 3}, page_number=1, per_page=5),
+            ([1, 2], get_pagination_data(2, 1, 1, 5))
+        )
+        self.assertEqual(
+            self.gallery.get_page(
+                objects=[1, 3, 2], filter_params={'remove': 3}, page_number=10, per_page=5),
+            ([], get_pagination_data(2, 10, 1, 5, errors=True))
+        )
 
 
 if __name__ == '__main__':
